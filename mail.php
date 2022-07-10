3be27fdd1feab3e651f6d7845a2af88dfd09dafc
“saenuruki” <sae.webwork@gmail.com>
2022-07-10 19:31:24 -0400

    update new portfolio

--- mail.php
+++ mail.php
@@ -28,7 +28,7 @@ if ($name == "") {
     $msg['field'] = "contact-message";
     $msg['code'] = FALSE;
 } else {
-    $to = 'contact@example.com';
+    $to = 'sae.tokodai@gmail.com';
     $subject = 'inbio Contact Query';
     $_message = '<html><head></head><body>';
     $_message .= '<p>Name: ' . $name . '</p>';
@@ -39,10 +39,8 @@ if ($name == "") {
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From:  inbio <contact@example.com>' . "\r\n";
-    $headers .= 'cc: contact@example.com' . "\r\n";
-    $headers .= 'bcc: contact@example.com' . "\r\n";
-    mail($to, $subject, $_message, $headers, '-f contact@example.com');
+    $headers .= 'From:  saenuruki Website <sae.tokodai@gmail.com>' . "\r\n";
+    mail($to, $subject, $_message, $headers, '-f sae.tokodai@gmail.com');
 
     $msg['success'] = "\n Email has been sent successfully.";
     $msg['code'] = TRUE;
